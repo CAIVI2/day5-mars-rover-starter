c45a11538f8cb1467d6bd3585e6c6a0409877c27
VICTOR <victor.cai@oocl.com>
2025-09-06 11:10:55 +0800

    test: should_return_y_plus_when_input_moveForward_N

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -2,9 +2,18 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MarsRoverTest {
     @Test
     public void should_return_y_plus_when_input_moveForward_N() {
+        String command = "M";
 
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        marsRover.executeCommand(command);
+
+        assertEquals(0, marsRover.getX());
+        assertEquals(1, marsRover.getY());
+        assertEquals("N", marsRover.getDirection());
     }
 }
